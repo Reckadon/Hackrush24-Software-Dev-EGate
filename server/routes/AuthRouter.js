74802f84c52a3f6dbb19151ab47b27e2fa2a85ad
Karan Gandhi <karangandhi.programming@gmail.com>
2024-04-14 02:45:30 +0530

    Added more routes, auth features, QR features

--- server/routes/AuthRouter.js
+++ server/routes/AuthRouter.js
@@ -1,14 +1,17 @@
 import express from "express";
-import { createResidentUser } from "../utils/AuthUtils.js";
+import { createResidentUser, getUser } from "../utils/AuthUtils.js";
 
 const router = express.Router();
+const SECURITY_LOGIN = "abc";
+const SECURITY_PASSWORD = "123";
 
 router.get("/", (req, res) => {
   res.send("Hello world from auth");
 });
 
 router.post("/createResidentUser", async (req, res) => {
-  const userInfo = req.body;
+  // console.log(req.body);
+  const userInfo = req.body.data;
   res.json({
     user: await createResidentUser(
       userInfo.uuid,
@@ -18,6 +21,34 @@ router.post("/createResidentUser", async (req, res) => {
       userInfo.userType,
       userInfo.graduation
     ),
+  });
+});
+
+router.get("/users/:uuid", async (req, res) => {
+  const userid = req.params.uuid;
+  try {
+    res.json({
+      user: await getUser(userid),
+    });
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(400);
+  }
+});
+
+router.post("/createGuestUser", async (req, res) => {
+  // console.log(req.body);
+  const userInfo = req.body.data;
+  res.json({
+    // user: ,
+  });
+});
+
+router.post("/securityLogin", (req, res) => {
+  // console.log(req.body);
+  const userInfo = req.body;
+  res.json({
+    success: userInfo.login === SECURITY_LOGIN && userInfo.password === SECURITY_PASSWORD,
   });
 });
 
